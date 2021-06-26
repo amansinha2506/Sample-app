54aa8615c3d79f7228bfbfeb69e51060492995e9
amansinha2506 <aman.sinha@splashlearn.com>
2021-06-26 21:16:58 +0530

    Finish Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
+  # StaticPagesController inherit from ApplicationController
+  # home is a methos inside StaticPagesController class
   def home
   end
 
   def help
   end
+
+  def help
+  end
+
+
 end
